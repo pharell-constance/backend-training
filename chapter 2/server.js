1f47d6c76c67d8d7c056188585f2e243d323788a
pharell-constance <pharell.constance@laplateforme.io>
2025-11-27 15:54:40 +0100

    first post method

--- chapter 2/server.js
+++ chapter 2/server.js
@@ -4,14 +4,38 @@ const express = require('express');
 const app = express();
 const PORT = 8383;
 
-app.get('/', (req, res) =>{
-    res.send('<h1>hellooo</h1>')
+let data = {
+    name:'Pharell',
+
+}
+
+
+//type 1 - website endpoints (pour renvoyer du html arrive typiquement quand un utilisateur entre une url dans le navigateur)
+app.get('/', (req, res) => {
+    res.send(`
+        <body style="background:pink; color:blue">
+            <h1>DATA</h1>
+            <p>${JSON.stringify(data)}</p>
+        </body>`
+    );
 });
 
 app.get('/dashboard',(req, res)=>{
     console.log("l'endpoint du dashboard marche!!!");
-    res.send('salut !');
+    res.send('<h1>dashboard</h1>');
 })
 
+//type 2 - api endpoints
+
+//CRUD-method - create-post read-get update-put and delete-delete
+
+app.get('/api/data', (req, res)=> {
+    console.log("celui là est pour les data");
+    res.send(data)
+})
+
+app.post('/api/data', (req, res) => {
+    const newEntry = req.body
+})
 
 app.listen(PORT, () => console.log(`server has started on: ${PORT}`));
